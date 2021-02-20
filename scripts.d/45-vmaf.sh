1ada5eb96ff2d1025dbe25a09f91c92b3de5d9ea
nanake <nanake@users.noreply.github.com>
2021-02-20 12:33:49 +0700

    vmaf: 🏗 build static library alone again and fix 🩹 linking into ffmpeg
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/45-vmaf.sh
+++ scripts.d/45-vmaf.sh
@@ -21,6 +21,7 @@ ffbuild_dockerbuild() {
     local myconf=(
         --prefix="$FFBUILD_PREFIX"
         --buildtype=release
+        --default-library=static
         -Denable_tests=false
         -Denable_docs=false
         -Denable_avx512=true
@@ -39,14 +40,7 @@ ffbuild_dockerbuild() {
     ninja -j"$(nproc)"
     ninja install
 
-    sed -i 's/Libs.private:/Libs.private: -lstdc++/' "$FFBUILD_PREFIX"/lib/pkgconfig/libvmaf.pc
-
-    if [[ $TARGET == win* ]]; then
-        rm "$FFBUILD_PREFIX"/bin/libvmaf* "$FFBUILD_PREFIX"/lib/libvmaf.dll.a
-    else
-        echo "Unknown target"
-        return -1
-    fi
+    sed -i 's/Libs.private.*/& -lstdc++/; t; $ a Libs.private: -lstdc++' "$FFBUILD_PREFIX"/lib/pkgconfig/libvmaf.pc
 
     cd ../..
     rm -rf vmaf
