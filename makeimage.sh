afde84ac931562536f6b42aff79c49f28e2d86a4
nanake <nanake@users.noreply.github.com>
2021-04-10 08:18:41 +0700

    Revert "🐋 Use docker buildx 🧱"
    This reverts commit 462c0190233f1ad23588a1a8237b8d198c0ad43b.

--- makeimage.sh
+++ makeimage.sh
@@ -3,9 +3,11 @@ set -xe
 cd "$(dirname "$0")"
 source util/vars.sh
 
-docker buildx build --tag "$BASE_IMAGE" images/base
-docker buildx build --build-arg GH_OWNER="$OWNER" --tag "$TARGET_IMAGE" "images/base-${TARGET}"
+export DOCKER_BUILDKIT=1
+
+docker build --tag "$BASE_IMAGE" images/base
+docker build --build-arg GH_OWNER="$OWNER" --tag "$TARGET_IMAGE" "images/base-${TARGET}"
 
 ./generate.sh "$TARGET" "$VARIANT" "${ADDINS[@]}"
 
-exec docker buildx build --tag "$IMAGE" .
+exec docker build --tag "$IMAGE" .
