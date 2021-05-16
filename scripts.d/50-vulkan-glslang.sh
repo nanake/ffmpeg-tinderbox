b5e31f0988d03f94f552366643085840aecf2f9e
nanake <nanake@users.noreply.github.com>
2021-05-17 08:43:54 +0900

    🧺 Add vulkan and glslang 🚀
    Signed-off-by: nanake <nanake@users.noreply.github.com>

---
+++ scripts.d/50-vulkan-glslang.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+VKGLS_WIN32="https://github.com/nanake/Vulkan-Loader/releases/download/v1.2.174/vulkan.glslang.win32.tar.xz"
+VKGLS_WIN64="https://github.com/nanake/Vulkan-Loader/releases/download/v1.2.174/vulkan.glslang.win64.tar.xz"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    if [[ $TARGET == win32 ]]; then
+        wget -O vkgls.tar.xz "$VKGLS_WIN32"
+    elif [[ $TARGET == win64 ]]; then
+        wget -O vkgls.tar.xz "$VKGLS_WIN64"
+    fi
+
+    tar xaf vkgls.tar.xz
+    rm vkgls.tar.xz
+    cd vulkan*
+
+    cp -r . "$FFBUILD_PREFIX"
+
+    ln -s "$FFBUILD_PREFIX"/lib/libvulkan-1.a "$FFBUILD_PREFIX"/lib/libvulkan.a
+}
+
+ffbuild_configure() {
+    echo --enable-vulkan --enable-libglslang
+}
+
+ffbuild_unconfigure() {
+    echo --disable-vulkan --disable-libglslang
+}
