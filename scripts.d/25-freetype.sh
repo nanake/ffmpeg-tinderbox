41f1e09e92730e492f54ff1b312725d921bec253
nanake <nanake@users.noreply.github.com>
2022-03-15 08:02:04 +0900

    🌀 freetype: use cmake 🥷
    Follow up from nanake/ffmpeg-tinderbox@c673b66
    
    It works fine with cmake, so there's something wrong with meson.
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/25-freetype.sh
+++ scripts.d/25-freetype.sh
@@ -12,24 +12,18 @@ ffbuild_dockerbuild() {
     rm ft.tar.xz
     cd freetype*
 
-    local myconf=(
-        --prefix="$FFBUILD_PREFIX"
-        --disable-shared
-        --enable-static
-    )
+    mkdir build && cd build
 
-    if [[ $TARGET == win* ]]; then
-        myconf+=(
-            --host="$FFBUILD_TOOLCHAIN"
-        )
-    else
-        echo "Unknown target"
-        return -1
-    fi
-
-    ./configure "${myconf[@]}"
-    make -j$(nproc)
-    make install
+    cmake \
+        -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DFT_DISABLE_{BROTLI,BZIP2,HARFBUZZ,PNG,ZLIB}=TRUE \
+        -GNinja \
+        ..
+    ninja -j$(nproc)
+    ninja install
 }
 
 ffbuild_configure() {
