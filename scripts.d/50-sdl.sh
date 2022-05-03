1dcbfbecc45a25b9ed15224f8ce5bd67c7aa4815
nanake <nanake@users.noreply.github.com>
2022-05-03 15:43:34 +0900

    🪡 SDL: patch to fix build bustage 🎉
    Required for mirror/mingw-w64@f18d3a3b84ea1f8d8091a5ca3e4592465d2d1982
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/50-sdl.sh
+++ scripts.d/50-sdl.sh
@@ -7,9 +7,18 @@ ffbuild_enabled() {
     return 0
 }
 
+ffbuild_dockerstage() {
+    to_df "RUN --mount=src=${SELF},dst=/stage.sh --mount=src=patches/sdl,dst=/patches run_stage /stage.sh"
+}
+
 ffbuild_dockerbuild() {
     git-mini-clone "$SDL_REPO" "$SDL_COMMIT" sdl
     cd sdl
+
+    for patch in /patches/*.patch; do
+        echo "Applying $patch"
+        git am < "$patch"
+    done
 
     mkdir build && cd build
 
