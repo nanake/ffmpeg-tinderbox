36bd26f3350e747755ef35b8790eccebf9887236
nanake <nanake@users.noreply.github.com>
2022-08-18 16:55:06 +0900

    Add kvazaar
    Signed-off-by: nanake <nanake@users.noreply.github.com>

---
+++ scripts.d/50-kvazaar.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+KVAZAAR_REPO="https://github.com/ultravideo/kvazaar.git"
+KVAZAAR_COMMIT="76ce0c7716f593b5f4614aa864603684dd4fae3d"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$KVAZAAR_REPO" "$KVAZAAR_COMMIT" kvazaar
+    cd kvazaar
+
+    ./autogen.sh
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-shared
+        --enable-static
+        --with-pic
+    )
+
+    if [[ $TARGET == win* ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    ./configure "${myconf[@]}"
+    make -j$(nproc)
+    make install
+
+    echo "Cflags.private: -DKVZ_STATIC_LIB" >> "$FFBUILD_PREFIX"/lib/pkgconfig/kvazaar.pc
+}
+
+ffbuild_configure() {
+    echo --enable-libkvazaar
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libkvazaar
+}
