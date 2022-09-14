2ea65416c95fa72dd0871b8d0e6c25cbc2ea4a44
nanake <nanake@users.noreply.github.com>
2022-09-14 15:55:58 +0900

    libxml2: migrate to cmake 🥷
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/20-libxml2.sh
+++ scripts.d/20-libxml2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LIBXML2_REPO="https://gitlab.gnome.org/GNOME/libxml2.git"
+LIBXML2_REPO="https://github.com/GNOME/libxml2.git"
 LIBXML2_COMMIT="1d4f5d24ac3976012ab1f5b811385e7b00caaecf"
 
 ffbuild_enabled() {
@@ -8,34 +8,23 @@ ffbuild_enabled() {
 }
 
 ffbuild_dockerbuild() {
-    # pinned to last known good
-    # build bustage on ubuntu:kinetic (mingw-w64 10.0 AND gcc-10.3)
-    if [[ $TARGET == win32 ]]; then
-        LIBXML2_COMMIT="6a5c88cc5e009def0b6d9706019d799fce49faa1"
-    fi
-
     git-mini-clone "$LIBXML2_REPO" "$LIBXML2_COMMIT" libxml2
     cd libxml2
 
-    local myconf=(
-        --prefix="$FFBUILD_PREFIX"
-        --without-{catalog,debug,docbook,history,html,python}
-        --disable-{shared,maintainer-mode}
-        --enable-static
-    )
+    mkdir build && cd build
 
-    if [[ $TARGET == win* ]]; then
-        myconf+=(
-            --host="$FFBUILD_TOOLCHAIN"
-        )
-    else
-        echo "Unknown target"
-        return -1
-    fi
+    cmake \
+        -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DLIBXML2_WITH_{CATALOG,DEBUG,HTML,HTTP,ICONV,LZMA,PROGRAMS,PYTHON,TESTS,ZLIB}=OFF \
+        -GNinja \
+        ..
+    ninja -j$(nproc)
+    ninja install
 
-    ./autogen.sh "${myconf[@]}"
-    make -j$(nproc)
-    make install
+    sed -i "s|^prefix=.*|prefix=${FFBUILD_PREFIX}|" "$FFBUILD_PREFIX"/lib/pkgconfig/libxml-2.0.pc
 }
 
 ffbuild_configure() {
