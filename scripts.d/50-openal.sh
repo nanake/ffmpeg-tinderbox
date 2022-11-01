f46076896b75a392afd03deb130d9ad53d1c6e9d
nanake <nanake@users.noreply.github.com>
2022-11-01 12:21:01 +0900

    Add OpenAL
    Signed-off-by: nanake <nanake@users.noreply.github.com>

---
+++ scripts.d/50-openal.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+OPENAL_REPO="https://github.com/kcat/openal-soft.git"
+OPENAL_COMMIT="bc7f5260475586b833a568c7b3ac48ac9f89be3b"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$OPENAL_REPO" "$OPENAL_COMMIT" openal
+    cd openal
+
+    mkdir cmbuild && cd cmbuild
+
+    cmake \
+        -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DLIBTYPE=STATIC \
+        -DALSOFT_{EXAMPLES,UTILS,INSTALL_{AMBDEC_PRESETS,CONFIG,EXAMPLES,HRTF_DATA,UTILS}}=OFF \
+        -GNinja \
+        ..
+    ninja -j$(nproc)
+    ninja install
+
+    sed -i 's/Libs.private.*/& -lstdc++ -lole32/' "$FFBUILD_PREFIX"/lib/pkgconfig/openal.pc
+}
+
+ffbuild_configure() {
+    echo --enable-openal
+}
+
+ffbuild_unconfigure() {
+    echo --disable-openal
+}
