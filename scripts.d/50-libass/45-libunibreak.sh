e64e72e9554bb912cc715d4cf2cf5659524968b0
nanake <nanake@users.noreply.github.com>
2023-07-03 13:14:33 +0900

    Add libunibreak dependency
    in order to enable `WRAP_UNICODE` feature support
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

---
+++ scripts.d/50-libass/45-libunibreak.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+LIBUNIBREAK_REPO="https://github.com/adah1972/libunibreak.git"
+LIBUNIBREAK_COMMIT="8d0dcf3462bd3f3c48676fce29131dbc4824352b"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$LIBUNIBREAK_REPO" "$LIBUNIBREAK_COMMIT" libunibreak
+    cd libunibreak
+
+    ./bootstrap
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --enable-static
+        --disable-shared
+    )
+
+    if [[ $TARGET =~ ^(ucrt64|win(64|32))$ ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    ./configure "${myconf[@]}"
+    make -j$(nproc)
+    make install
+}
