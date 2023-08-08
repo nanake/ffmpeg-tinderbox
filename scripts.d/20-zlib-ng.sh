19422cc29c1228c6a88303fb494c36c7c9324589
nanake <nanake@users.noreply.github.com>
2023-08-08 15:37:23 +0900

    zlib-ng as a compat replacement for zlib
    Signed-off-by: nanake <nanake@users.noreply.github.com>

---
+++ scripts.d/20-zlib-ng.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ZLIB_REPO="https://github.com/zlib-ng/zlib-ng.git"
+ZLIB_COMMIT="73bbb54cf6686a81710a1326c4cf1cfee9a49784"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$ZLIB_REPO" "$ZLIB_COMMIT" zlib
+    cd zlib
+
+    mkdir build && cd build
+
+    cmake \
+        -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DZLIB_COMPAT=ON \
+        -DZLIB{,NG}_ENABLE_TESTS=OFF \
+        -DWITH_GTEST=OFF \
+        -GNinja \
+        ..
+    ninja -j$(nproc)
+    ninja install
+}
+
+ffbuild_configure() {
+    echo --enable-zlib
+}
+
+ffbuild_unconfigure() {
+    echo --disable-zlib
+}
