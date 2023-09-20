3ae1e19d203a9a8b871aae67038937f806b97198
nanake <nanake@users.noreply.github.com>
2023-09-20 22:26:02 +0900

    Bump OpenSSL to openssl-3.2.0-alpha1
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/50-srt/25-openssl.sh
+++ scripts.d/50-srt/25-openssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OPENSSL_REPO="https://github.com/openssl/openssl.git"
-OPENSSL_COMMIT="openssl-3.1.2"
+OPENSSL_COMMIT="openssl-3.2.0-alpha1"
 
 ffbuild_enabled() {
     return 0
@@ -14,7 +14,7 @@ ffbuild_dockerbuild() {
     local myconf=(
         threads
         zlib
-        no-{legacy,module,shared,tests}
+        no-{apps,legacy,module,shared,ssl,tests}
         enable-{camellia,ec,srp}
         --prefix="$FFBUILD_PREFIX"
     )
