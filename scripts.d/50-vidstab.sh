671ac3d029c72133fb34ceeed7970d1329de9760
nanake <nanake@users.noreply.github.com>
2023-10-27 11:56:32 +0900

    vidstab: remove patch
    FFmpeg/FFmpeg@2b300eb
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/50-vidstab.sh
+++ scripts.d/50-vidstab.sh
@@ -8,18 +8,9 @@ ffbuild_enabled() {
     return 0
 }
 
-ffbuild_dockerstage() {
-    to_df "RUN --mount=src=${SELF},dst=/stage.sh --mount=src=patches/vidstab,dst=/patches run_stage /stage.sh"
-}
-
 ffbuild_dockerbuild() {
     git-mini-clone "$VIDSTAB_REPO" "$VIDSTAB_COMMIT" vidstab
     cd vidstab
-
-    for patch in /patches/*.patch; do
-        echo "Applying $patch"
-        git am < "$patch"
-    done
 
     mkdir build && cd build
 
