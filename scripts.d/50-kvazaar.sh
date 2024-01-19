877b75c21fa19d314f64bd55f70984af68fb29de
nanake <nanake@users.noreply.github.com>
2024-01-19 12:14:08 +0900

    kvazaar: build with cmake
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/50-kvazaar.sh
+++ scripts.d/50-kvazaar.sh
@@ -11,28 +11,20 @@ ffbuild_dockerbuild() {
     git-mini-clone "$KVAZAAR_REPO" "$KVAZAAR_COMMIT" kvazaar
     cd kvazaar
 
-    ./autogen.sh
+    mkdir kvzbuild && cd kvzbuild
 
-    local myconf=(
-        --prefix="$FFBUILD_PREFIX"
-        --disable-shared
-        --enable-static
-        --with-pic
-    )
+    cmake \
+        -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DBUILD_SHARED_LIBS=OFF \
+        -D{BUILD_TESTS,KVZ_BUILD_SHARED_LIBS}=OFF \
+        -GNinja \
+        ..
+    ninja -j"$(nproc)"
+    ninja install
 
-    if [[ $TARGET == win* ]]; then
-        myconf+=(
-            --host="$FFBUILD_TOOLCHAIN"
-        )
-    else
-        echo "Unknown target"
-        return -1
-    fi
-
-    ./configure "${myconf[@]}"
-    make -j"$(nproc)"
-    make install
-
+    mv "$FFBUILD_PREFIX"/{share,lib}/pkgconfig/kvazaar.pc
     echo "Cflags.private: -DKVZ_STATIC_LIB" >> "$FFBUILD_PREFIX"/lib/pkgconfig/kvazaar.pc
 }
 
