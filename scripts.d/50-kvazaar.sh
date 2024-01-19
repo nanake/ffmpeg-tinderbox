b92cdd708cf564545310a269e27ecadce32f2a57
nanake <nanake@users.noreply.github.com>
2024-01-19 13:14:33 +0900

    Revert "kvazaar: build with cmake"
    This reverts commit 877b75c21fa19d314f64bd55f70984af68fb29de.
    
    libkvazaar encoder produces 0 byte output if built with cmake.

--- scripts.d/50-kvazaar.sh
+++ scripts.d/50-kvazaar.sh
@@ -11,20 +11,28 @@ ffbuild_dockerbuild() {
     git-mini-clone "$KVAZAAR_REPO" "$KVAZAAR_COMMIT" kvazaar
     cd kvazaar
 
-    mkdir kvzbuild && cd kvzbuild
+    ./autogen.sh
 
-    cmake \
-        -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
-        -DBUILD_SHARED_LIBS=OFF \
-        -D{BUILD_TESTS,KVZ_BUILD_SHARED_LIBS}=OFF \
-        -GNinja \
-        ..
-    ninja -j"$(nproc)"
-    ninja install
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-shared
+        --enable-static
+        --with-pic
+    )
 
-    mv "$FFBUILD_PREFIX"/{share,lib}/pkgconfig/kvazaar.pc
+    if [[ $TARGET == win* ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    ./configure "${myconf[@]}"
+    make -j"$(nproc)"
+    make install
+
     echo "Cflags.private: -DKVZ_STATIC_LIB" >> "$FFBUILD_PREFIX"/lib/pkgconfig/kvazaar.pc
 }
 
