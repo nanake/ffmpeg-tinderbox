75e42fb404dabf036211316d944013fd97db5d1f
nanake <nanake@users.noreply.github.com>
2024-02-22 13:37:37 +0900

    zvbi: remove workaround
    fixed in zapping-vbi/zvbi#45
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/50-zvbi.sh
+++ scripts.d/50-zvbi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ZVBI_REPO="https://github.com/zapping-vbi/zvbi.git"
-ZVBI_COMMIT="b4cef506bbaa7b8bffb07a5566328c7766dbab42"
+ZVBI_COMMIT="e0ff90c338500fd468c7925db85bed4b1b2d1e72"
 
 ffbuild_enabled() {
     return 0
@@ -10,12 +10,6 @@ ffbuild_enabled() {
 ffbuild_dockerbuild() {
     git-mini-clone "$ZVBI_REPO" "$ZVBI_COMMIT" zvbi
     cd zvbi
-
-    # workaround for C type errors in GCC 14
-    # GCC 14 turns error on implicit function declarations by default
-    # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=91092
-    # this basically restoring the GCC 13 behavior
-    export CFLAGS="$CFLAGS -Wno-error=implicit-function-declaration"
 
     autoreconf -i
 
