e9f293290ba621213d043ed23cb9c6b984122457
nanake <nanake@users.noreply.github.com>
2024-03-02 16:36:50 +0900

    Omit DOCKER_BUILDKIT env from makeimage.sh
    BuildKit is the default builder on Docker Engine v23.0 and later
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- makeimage.sh
+++ makeimage.sh
@@ -3,8 +3,6 @@ set -xe
 cd "$(dirname "$0")"
 source util/vars.sh
 
-export DOCKER_BUILDKIT=1
-
 docker build --tag "$TARGET_IMAGE" "images/base-${TARGET}"
 
 ./generate.sh "$TARGET" "$VARIANT" "${ADDINS[@]}"
