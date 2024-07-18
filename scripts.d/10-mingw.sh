4fdbbb4f90cc81549ba8b67eb55af521202bc412
nanake <nanake@users.noreply.github.com>
2024-07-18 14:00:18 +0900

    mingw: follow up for mingw-crt-12.0.0-1.fc41
    - UCRT is now the default CRT runtime for i686-w64-mingw32
    - winpthreads is up-to-date, don't rebuild
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/10-mingw.sh
+++ scripts.d/10-mingw.sh
@@ -18,16 +18,8 @@ ffbuild_dockerfinal() {
 }
 
 ffbuild_dockerbuild() {
-    # deps build bustage with undefined reference to `_imp___stat32i64'
-    # regressed by https://github.com/mingw-w64/mingw-w64/commit/4f96852
-    if [[ $TARGET == win32 ]]; then
-        MINGW_COMMIT="cc6afd4919d1721c19a1598570be22a00b04bae9"
-    fi
-
     git-mini-clone "$MINGW_REPO" "$MINGW_COMMIT" mingw
-    cd mingw
-
-    cd mingw-w64-headers
+    cd mingw/mingw-w64-headers
 
     unset CFLAGS CXXFLAGS LDFLAGS PKG_CONFIG_LIBDIR
 
@@ -37,26 +29,6 @@ ffbuild_dockerbuild() {
         --prefix="$SYSROOT/mingw"
         --host="$FFBUILD_TOOLCHAIN"
         --enable-idl
-    )
-
-    if [[ $TARGET != win64 ]]; then
-        myconf+=(
-            --with-default-msvcrt=msvcrt
-        )
-    fi
-
-    ./configure "${myconf[@]}"
-    make -j"$(nproc)"
-    make install DESTDIR="/opt/mingw"
-
-    cd ../mingw-w64-libraries/winpthreads
-
-    local myconf=(
-        --prefix="$SYSROOT/mingw"
-        --host="$FFBUILD_TOOLCHAIN"
-        --with-pic
-        --disable-shared
-        --enable-static
     )
 
     ./configure "${myconf[@]}"
