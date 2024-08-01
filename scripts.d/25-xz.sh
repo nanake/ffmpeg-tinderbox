7cdf2dfe7c417e905f1eb759afb28c7a1de523c6
nanake <nanake@users.noreply.github.com>
2024-08-01 11:18:55 +0900

    Revert "CVE-2024-3094: remove xz"
    This reverts commit 19b50a93c296ed1cb84a79e0f5925cc90d414b9c.
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

---
+++ scripts.d/25-xz.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+XZ_REPO="https://github.com/tukaani-project/xz.git"
+XZ_COMMIT="a4f2e20d8466369b1bb277c66f75c9e4ba9cc378"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$XZ_REPO" "$XZ_COMMIT" xz
+    cd xz
+
+    mkdir build && cd build
+
+    cmake \
+        -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DBUILD_{SHARED_LIBS,TESTING}=OFF \
+        -DENABLE_THREADS=posix \
+        -DCMAKE_USE_PTHREADS_INIT=ON \
+        -GNinja \
+        ..
+
+    ninja -j"$(nproc)"
+    ninja install
+}
+
+ffbuild_configure() {
+    echo --enable-lzma
+}
+
+ffbuild_unconfigure() {
+    echo --disable-lzma
+}
