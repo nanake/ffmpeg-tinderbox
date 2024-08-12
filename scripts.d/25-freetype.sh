7e1ce00dd47414f1d3856898a382e2d51efa2c8a
nanake <nanake@users.noreply.github.com>
2024-08-12 14:57:09 +0900

    Build FreeType with CMake
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/25-freetype.sh
+++ scripts.d/25-freetype.sh
@@ -11,28 +11,18 @@ ffbuild_dockerbuild() {
     git-mini-clone "$FREETYPE_REPO" "$FREETYPE_COMMIT" freetype
     cd freetype
 
-    ./autogen.sh
+    mkdir build && cd build
 
-    local myconf=(
-        --prefix="$FFBUILD_PREFIX"
-        --disable-shared
-        --enable-static
-    )
-
-    if [[ $TARGET == win* ]]; then
-        myconf+=(
-            --host="$FFBUILD_TOOLCHAIN"
-            PTHREAD_CFLAGS="-pthread"
-            PTHREAD_LIBS="-lpthread"
-        )
-    else
-        echo "Unknown target"
-        return -1
-    fi
-
-    ./configure "${myconf[@]}"
-    make -j"$(nproc)"
-    make install
+    cmake \
+        -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DFT_REQUIRE_ZLIB=ON \
+        -GNinja \
+        ..
+    ninja -j"$(nproc)"
+    ninja install
 }
 
 ffbuild_configure() {
