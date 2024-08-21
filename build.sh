f5197954a3b18f44b4bfbc9daff3b2ce79b59745
nanake <nanake@users.noreply.github.com>
2024-08-21 11:26:27 +0900

    [FFmpeg-Patch] Fix check for zmq prefix
    [skip ci]
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -66,6 +66,10 @@ cat <<EOF >"$BUILD_SCRIPT"
         git checkout "$FFMPEG_COMMIT"
     fi
 
+    if git apply --check /patches/*.patch 2> /dev/null; then
+        git apply /patches/*.patch
+    fi
+
     ./configure --prefix=/ffbuild/prefix --pkg-config-flags="--static" \$FFBUILD_TARGET_FLAGS $FF_CONFIGURE \
         --extra-cflags="$FF_CFLAGS" --extra-cxxflags="$FF_CXXFLAGS" \
         --extra-ldflags="$FF_LDFLAGS" --extra-libs="$FF_LIBS"
@@ -75,7 +79,7 @@ EOF
 
 [[ -t 1 ]] && TTY_ARG="-t" || TTY_ARG=""
 
-docker run --rm -i $TTY_ARG "${UIDARGS[@]}" -v $PWD/ffbuild:/ffbuild -v "$BUILD_SCRIPT":/build.sh "$IMAGE" bash /build.sh
+docker run --rm -i $TTY_ARG "${UIDARGS[@]}" -v "$PWD/ffbuild":/ffbuild -v "$PWD/patches/ffmpeg":/patches -v "$BUILD_SCRIPT":/build.sh "$IMAGE" bash /build.sh
 
 mkdir -p artifacts
 ARTIFACTS_PATH="$PWD/artifacts"
