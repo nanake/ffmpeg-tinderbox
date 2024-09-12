5f543564f5b422eff1ff0b8d52bc865c25b85c4d
nanake <nanake@users.noreply.github.com>
2024-09-13 08:42:30 +0900

    Pin rav1e to p20240409
    for Windows backward compatibility
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/50-rav1e.sh
+++ scripts.d/50-rav1e.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RAV1E_SRC_PREFIX="https://github.com/xiph/rav1e/releases/download/p20240612"
+RAV1E_SRC_PREFIX="https://github.com/xiph/rav1e/releases/download/p20240409"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
